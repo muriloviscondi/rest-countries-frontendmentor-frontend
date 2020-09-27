07631001317a9232b80d603f9632809786f79ed4
muriloviscondi <muriloviscondi@gmail.com>
2020-09-27 13:54:57 -0300

    criando grid details

--- js/app.js
+++ js/app.js
@@ -10,8 +10,32 @@ async function fetchCountries() {
   const data = await res.json();
 
   countries = data.map((country) => {
-    const { name, alpha3Code, capital, population, region, flag } = country;
-    return { name, alpha3Code, capital, population, region, flag };
+    const {
+      nativeName,
+      name,
+      alpha3Code,
+      population,
+      region,
+      subregion,
+      capital,
+      flag,
+      topLevelDomain,
+      currencies,
+      languages,
+    } = country;
+    return {
+      nativeName,
+      name,
+      alpha3Code,
+      population,
+      region,
+      subregion,
+      capital,
+      flag,
+      topLevelDomain,
+      currencies,
+      languages,
+    };
   });
 
   countriesFilter = [...countries];
@@ -22,6 +46,7 @@ async function fetchCountries() {
 function render() {
   listCountries();
   filteredCountries();
+  showCountry();
 }
 
 function listCountries() {
@@ -31,30 +56,7 @@ function listCountries() {
   divCountries.innerHTML = "";
 
   countriesFilter.forEach((country) => {
-    const cards = `
-      <div class="card">
-        <div class="flag">
-          <a href="description/${country.alpha3Code.toLowerCase()}">
-            <img src="${country.flag}" alt="${country.name}" title="${
-      country.name
-    }" />
-          </a>
-        </div>
-
-        <div class="description">
-          <div class="description__title">
-            <h2>${country.name}</h2>
-          </div>
-          <div class="description__country">
-            <p><strong>Population:</strong> ${country.population.toLocaleString(
-              "pt-BR"
-            )}</p>
-            <p><strong>Region:</strong> ${country.region}</p>
-            <p><strong>Capital:</strong> ${country.capital}</p>
-          </div>
-        </div>
-      </div>
-    `;
+    const cards = innerCards(country);
 
     cardsCountriesHTML += cards;
   });
@@ -81,4 +83,99 @@ function searchingCountries(value) {
   });
 
   render();
+}
+
+function showCountry() {
+  const modal = document.querySelector("#modal");
+  const close = document.querySelector("#close");
+  const cards = document.querySelectorAll(".flag");
+
+  let modalContent = document.querySelector("#modal__content");
+
+  cards.forEach((card) => {
+    card.addEventListener("click", () => {
+      modalContent.innerHTML = "";
+
+      const id = card.getAttribute("id");
+      modal.classList.add("active");
+
+      const descriptionCountry = countries.find((country) => {
+        return country.alpha3Code.toLowerCase() === id;
+      });
+
+      modalContent.innerHTML = innerDescription(descriptionCountry);
+      console.log(descriptionCountry);
+    });
+  });
+
+  close.addEventListener("click", () => {
+    modalContent.includes = "";
+    modal.classList.remove("active");
+  });
+}
+
+function innerCards(value) {
+  const card = `
+    <div class="card">
+      <div id=${value.alpha3Code.toLowerCase()} class="flag">
+        <a>
+          <img src="${value.flag}" alt="${value.name}" title="${value.name}" />
+        </a>
+      </div>
+
+      <div class="description">
+        <div class="description__title">
+          <h2>${value.name}</h2>
+        </div>
+        <div class="description__value">
+          <p><strong>Population:</strong> ${value.population.toLocaleString(
+            "pt-BR"
+          )}</p>
+          <p><strong>Region:</strong> ${value.region}</p>
+          <p><strong>Capital:</strong> ${value.capital}</p>
+        </div>
+      </div>
+    </div>
+  `;
+  return card;
+}
+
+function innerDescription(value) {
+  const card = `
+    <div class="card">
+      <div id=${value.alpha3Code.toLowerCase()} class="flag">
+        <a>
+          <img src="${value.flag}" alt="${value.name}" title="${value.name}" />
+        </a>
+      </div>
+
+      <div class="description">
+        <div id="description-title" class="description__title">
+          <h2>${value.name}</h2>
+          <div class="description__grid-content">
+            <div class="description__value">
+              <p><strong>Native Name:</strong> ${value.nativeName}</p>
+              <p><strong>Population:</strong> ${value.population.toLocaleString(
+                "pt-BR"
+              )}</p>
+              <p><strong>Region:</strong> ${value.region}</p>
+              <p><strong>Sub Region:</strong> ${value.subregion}</p>
+              <p><strong>Capital:</strong> ${value.capital}</p>
+            </div>
+
+            <div class="description__value">
+              <p><strong>Top Level Domain:</strong> ${value.topLevelDomain}</p>
+              <p><strong>currencies:</strong> ${value.currencies.name}</p>
+              <p><strong>Region:</strong>
+              ${value.languages.map((language) => {
+                return ` <span>${language.name}</span>`;
+              })}
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  `;
+  return card;
 }
